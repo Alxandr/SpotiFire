a9468261775de8141b33db3344aa2fc5b4c5c789
Alxandr <alxandr@alxandr.me>
2013-04-30 00:04:32 +0200

    Added a WPF test.

---
+++ SpotiFire.WpfTest/BASSPlayer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Un4seen.Bass;
+
+namespace SpotiFire.WpfTest
+{
+    class BASSPlayer
+    {
+        private BASSBuffer basbuffer = null;
+        private STREAMPROC streamproc = null;
+
+        public int EnqueueSamples(int channels, int rate, byte[] samples, int frames)
+        {
+            int consumed = 0;
+            if (basbuffer == null)
+            {
+                Bass.BASS_Init(-1, rate, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero);
+                basbuffer = new BASSBuffer(0.5f, rate, channels, 2);
+                streamproc = new STREAMPROC(Reader);
+                Bass.BASS_ChannelPlay(
+                    Bass.BASS_StreamCreate(rate, channels, BASSFlag.BASS_DEFAULT, streamproc, IntPtr.Zero),
+                    false
+                    );
+            }
+
+            if (basbuffer.Space(0) > samples.Length)
+            {
+                basbuffer.Write(samples, samples.Length);
+                consumed = frames;
+            }
+
+            return consumed;
+        }
+
+        private int Reader(int handle, IntPtr buffer, int length, IntPtr user)
+        {
+            return basbuffer.Read(buffer, length, user.ToInt32());
+        }
+
+        public void Stop()
+        {
+            // In real world usage you must remember to free the BASS stream if not reusing it!
+            basbuffer.Clear();
+        }
+
+        public float Volume
+        {
+            get
+            {
+                return Bass.BASS_GetVolume();
+            }
+            set
+            {
+                Bass.BASS_SetVolume(value);
+            }
+        }
+    }
+}
---
+++ SpotiFire.WpfTest/MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpotiFire.WpfTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Session session;
+        
+        static BASSPlayer player = new BASSPlayer();
+
+        private PlaylistContainer pc;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SetupSession()
+        {
+            session.MusicDelivered += session_MusicDeliverd;
+        }
+
+        void session_MusicDeliverd(Session sender, MusicDeliveryEventArgs e)
+        {
+            e.ConsumedFrames = player.EnqueueSamples(e.Channels, e.Rate, e.Samples, e.Frames);
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            session = await Spotify.Task;
+            SetupSession();
+            while (true)
+            {
+                var login = new Login(session);
+                var result = login.ShowDialog();
+                if (result.HasValue && result.Value)
+                    break;
+                else if (result.HasValue)
+                {
+                    Application.Current.Shutdown();
+                    return;
+                }
+            }
+
+            pc = await session.PlaylistContainer;
+            List<Playlist> playlists = new List<Playlist>(pc.Playlists.Count);
+            foreach (var playlist in pc.Playlists)
+            {
+                playlists.Add(await playlist);
+            }
+
+            Playlists.ItemsSource = playlists;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // perform search
+            var term = Search.Text;
+            var search = await session.SearchTracks(term, 0, 100);
+
+            List<Track> tracks = new List<Track>();
+            foreach (var t in search.Tracks)
+                tracks.Add(await t);
+
+            Songs.ItemsSource = tracks;
+        }
+
+        private async void Playlists_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count == 1)
+            {
+                var itm = e.AddedItems[0] as Playlist;
+                if (itm == null) return;
+
+                await itm;
+                List<Track> tracks = new List<Track>();
+                foreach (var t in itm.Tracks)
+                    tracks.Add(await t);
+
+                Songs.ItemsSource = tracks;
+            }
+        }
+
+        private async void TextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock tb = sender as TextBlock;
+            if (tb == null) return;
+            // click song
+            if (e.ClickCount == 2) // double
+            {
+                var track = tb.DataContext as Track;
+                if (track == null)
+                    return;
+
+                await track;
+                session.PlayerUnload();
+                session.PlayerLoad(track);
+                session.PlayerPlay();
+            }
+        }
+    }
+}
