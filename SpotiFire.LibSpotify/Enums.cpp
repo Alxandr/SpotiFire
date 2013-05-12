9c04426bba2026eb4e1721b4af300c2581be25d8
Alxandr <alxandr@alxandr.me>
2013-05-12 23:40:52 +0200

    Moved type into namespace
    Also created new package and published, hence version-bump

--- SpotiFire.LibSpotify/Enums.cpp
+++ SpotiFire.LibSpotify/Enums.cpp
@@ -4,21 +4,25 @@
 
 using namespace System::Runtime::CompilerServices;
 
-///-------------------------------------------------------------------------------------------------
-/// <summary>	The ErrorExtension class containing all the extension method for the Error Enum. </summary>
-///
-/// <remarks>	EraYaN, 30.04.2013 </remarks>
-///-------------------------------------------------------------------------------------------------
-[ExtensionAttribute]
-public ref class ErrorExtensions abstract sealed {
-	public:        
-		///-------------------------------------------------------------------------------------------------
-		/// <summary>	Using libspotify to convert Error to a nice string in English. </summary>
-		///
-		/// <remarks>	EraYaN, 30.04.2013 </remarks>
-		///-------------------------------------------------------------------------------------------------
-		[ExtensionAttribute]
-		static String ^Message(SpotiFire::Error _error) {				
-			return UTF8(sp_error_message((sp_error)_error));				
-		}
-};
+namespace SpotiFire {
+
+	///-------------------------------------------------------------------------------------------------
+	/// <summary>	The ErrorExtension class containing all the extension method for the Error Enum. </summary>
+	///
+	/// <remarks>	EraYaN, 30.04.2013 </remarks>
+	///-------------------------------------------------------------------------------------------------
+	[ExtensionAttribute]
+	public ref class ErrorExtensions abstract sealed {
+		public:        
+			///-------------------------------------------------------------------------------------------------
+			/// <summary>	Using libspotify to convert Error to a nice string in English. </summary>
+			///
+			/// <remarks>	EraYaN, 30.04.2013 </remarks>
+			///-------------------------------------------------------------------------------------------------
+			[ExtensionAttribute]
+			static String ^Message(SpotiFire::Error _error) {				
+				return UTF8(sp_error_message((sp_error)_error));				
+			}
+	};
+
+}
